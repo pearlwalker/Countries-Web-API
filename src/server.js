4212b65c096694c8e0a2bada84b6febbff41944f
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-29 19:58:34 -0500

    Empty URL struct const

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const query = require('querystring');
-
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+
+};
 
 const parseBody = (request, response, handler) => {
   const requestBody = [];
