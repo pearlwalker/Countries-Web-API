a4996d27594ee8d915a63c8d52b1bcfaad4dc671
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-29 20:02:39 -0500

    Added index / 404 handling to URL struct

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': htmlHandler.getIndex,
+  notFound: jsonHandler.notFound,
 };
 
 const parseBody = (request, response, handler) => {
